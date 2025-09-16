96c337a9d0a7ee0b66d963de9b8af2594bb7baae
victorzx9 <victordsf2008@gmail.com>
2025-09-15 22:27:23 -0300

    atualização aula02 - 15/09

--- aula02/routes/web.php
+++ aula02/routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+ //   return view('welcome');
+// });
 
-Route::get('/paginateste', function () {
-    return view('teste');
-});
+// rotas para cliente
+Route::post('/cliente','App\Http\Controllers\ClienteController@store'); //cadastro
+Route::get('/cliente','App\Http\Controllers\ClienteController@index'); //visualização
+
